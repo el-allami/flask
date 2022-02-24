bc91ac433d693a29273c1c3730dfca919f2cc4bb
el-allami <elallami.ali.nasr@iisgalvanimi.edu.it>
2022-02-24 10:54:18 +0000

    continuazione progetto

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+import random
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -7,7 +8,18 @@ def hello_world():
 
 @app.route('/meteo', methods=['GET'])
 def meteo():
-    return render_template('pagina1.html')
+    # generazione numero casuale 
+    numero_casuale= random.randrange(1, 10)
+    # se il numero è minore di 2 
+    if numero_casuale <= 2:
+        return render_template('pagina1.html',previsione = 'pioggia' )
+    # se il numero è comreso tra 3 e 5 nuvoloso 
+    elif numero_casuale <=5:
+        return render_template('pagina1.html',previsione = 'nuvoloso' )
+    # se e maggiore di 5 sole
+    else:
+        return render_template('pagina1.html',previsione = 'sole' )
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
