a6ead4b0a980b2c47f4a31f0e1d04587207afdf5
el-allami <elallami.ali.nasr@iisgalvanimi.edu.it>
2022-03-09 13:51:56 +0000

    esercizi

--- esercizio2.py
+++ esercizio2.py
@@ -7,6 +7,7 @@
 #se sono corrette fornire un messaggio di benvenuto diverso a seconda del sesso 
 from flask import Flask, render_template, request
 app = Flask(__name__)
+lista = []
 
 @app.route('/', methods = ['GET'])
 def hello_world():
@@ -22,16 +23,24 @@ def data():
     password = request.args['password']
     Conferma_Password  = request.args['Conferma_Password']
     if password == Conferma_Password:
-        if Sex == 'M':
-            msg= 'BENVENUTO' + nome
-        elif Sex  == 'F':
-             msg= 'BENVENUTA' + nome
-        else:
-            msg = 'BENVENUT*' + nome
-        return render_template('welcome2.html',messaggio=msg)      
+       lista.append({'Name':nome,'username':username,'password':password,'Sex':Sex})
+       print(lista)
+       return render_template('login.html')
     else:
-        msg = 'PASSWORD NON COINCIDENTI'
-        return render_template('welcome2.html',messaggio=msg)
+        return render_template('error.html')
 
+@app.route('/login', methods = ['GET'])
+def login():
+    username_log = request.args['username']
+    password_log = request.args['password']
+    for utente in lista:
+        if utente['username'] == username_log and utente['password'] == password_log:
+            if utente['Sex'] =='M':
+
+              return render_template('welcome2.html',username=utente['Name'])
+            else:
+             return render_template('benvenuta.html',username=utente['Name'])
+    return render_template('error.html',messaggio='username o password errati')
+   
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
